41e71ad815e0e199fb07463598b738d9577cf937
Sunitha Selvan <sunitha98selvan@gmail.com>
2024-06-12 17:20:04 -0700

    Update hf_pipeline.py

--- app/hf_pipeline.py
+++ app/hf_pipeline.py
@@ -78,7 +78,7 @@ def main():
                 device="cuda",
             )
           
-        for input in inputs_rows:
+        for input in input_rows:
             label, prompt = input[0], input[1]
             message = prompt[0]['content']
             response = pipe(message)[0]['generated_text'][-1]
