f7d0785e20e67991aef72e9c53fcbb5e51ef11c6
Sunitha Selvan <sunitha98selvan@gmail.com>
2024-06-12 17:16:35 -0700

    Update hf_pipeline.py

--- app/hf_pipeline.py
+++ app/hf_pipeline.py
@@ -63,7 +63,7 @@ def main():
     test_ds = load_dataset(test_ds_path, split="test[:10]")
     test_ds = test_ds.to_pandas()
     
-    prompts = test_ds["messages"].tolist()
+    messages = test_ds["messages"].tolist()
     labels = test_ds["LABEL"].tolist()
     inputs = list(zip(labels, messages))
 
